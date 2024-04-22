aaaeee17b1099dc44e87bfa516e57aae689062a3
windevdev <163360237+windevdev@users.noreply.github.com>
2024-04-22 19:32:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 (function () {
 
-let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-  let firstLetter = names[i].charAt(0).toLowerCase();
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
